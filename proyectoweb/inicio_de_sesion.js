938bb9b596e0e52c25436b53c0e5007b84998ff8
GuadalfredoGames <74908796+GuadalfredoGames@users.noreply.github.com>
2021-05-12 21:32:10 +0200

    inicio de sesion logrado

--- proyectoweb/inicio_de_sesion.js
+++ proyectoweb/inicio_de_sesion.js
@@ -86,7 +86,7 @@ function controlar(){
 		  miFuncion(this);
 		}
 	  };
-	  xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml", true);
+	  xhr.open("GET", "https://guadalfredogames.github.io/publico/proyectoweb/validacionUsuarios.xml", true);
 	  xhr.send();
 	}
 
